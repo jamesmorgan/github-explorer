294816ad6df09ce637e5b212e0b1505e8058688d
Justin Smith <justin.smith@summitsystemsinc.com>
2013-05-20 19:29:08 -0500

    Fixing shell scripts to use the ~ wildcard for home.
    Signed-off-by: Justin Smith <justin.smith@summitsystemsinc.com>

--- install_github_projects.sh
+++ install_github_projects.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-pluginDir=/home/james/.local/share/cinnamon/applets/github-projects@morgan-design.com
+pluginDir=~/.local/share/cinnamon/applets/github-projects@morgan-design.com
 echo "The value of \"pluginDir\" is $pluginDir."
 
+echo "Removing old plugin"
 rm -r "$pluginDir"
-mkdir "$pluginDir"
 
 echo "Copying Plugin"
-cp -fr github-projects@morgan-design.com/* /home/james/.local/share/cinnamon/applets/github-projects@morgan-design.com
+cp -fr github-projects@morgan-design.com $pluginDir
