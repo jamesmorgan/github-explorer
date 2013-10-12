dbd3d9ba4ab1b4a58bf77f90f02d8b0dd0311f59
James Morgan <james.morgan.e@googlemail.com>
2013-10-12 17:38:31 +0100

    adding token to gitignore file

--- github-projects@morgan-design.com/github.js
+++ github-projects@morgan-design.com/github.js
@@ -58,6 +58,10 @@ function GitHub(options){
 	try {
 		this.httpSession = new Soup.SessionAsync();
 		this.httpSession.user_agent = this.user_agent;
+		this.httpSession.headers_append("Authorization: ", "tokan 935f9a8bb8107aa36fcc714701b9ccf2db07f627");
+		
+		//Authorization: token OAUTH-TOKEN
+		
 	} catch(e) {
 		throw 'GitHub: Creating SessionAsync failed: ' + e;
 	}
