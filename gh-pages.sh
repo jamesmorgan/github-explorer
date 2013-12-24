3df84432e660300f5cf553022e96480bea1fb673
James Morgan <james.morgan.e@googlemail.com>
2013-12-24 08:37:34 +0000

    GH Page script

--- gh-pages.sh
+++ gh-pages.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Starting Jykell"
-echo "Visit local at: "
+echo "Visit local at: TODO"
 echo "Visit remote at: http://jamesemorgan.github.io/github-explorer/"
 
 git checkout gh-pages
