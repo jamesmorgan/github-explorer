3b7ee14c90a628bdc6f2dd61711f6cfd60c8be5d
James Morgan <james.morgan.e@googlemail.com>
2015-04-12 20:33:28 +0100

    Adding warning to logoer

--- github-projects@morgan-design.com/Logger.js
+++ github-projects@morgan-design.com/Logger.js
@@ -7,12 +7,31 @@ function Logger(options){
     this.verboseLogging = options.verboseLogging || false;
 }
 
+/**
+ * Debug log message
+ *
+ * @param logMsg
+ */
 Logger.prototype.debug = function(logMsg){
 	if(this.verboseLogging){
 		global.log(this.uuid + "::" + logMsg);
 	}
 };
 
+/**
+ * Error log message
+ *
+ * @param error
+ */
 Logger.prototype.error = function(error) {
 	global.logError(this.uuid + ":: ERROR :: " + error);
+};
+
+/**
+ * Warning log message
+ *
+ * @param error
+ */
+Logger.prototype.warn = function(warning) {
+	global.logWarning(this.uuid + ":: WARN :: " + warning);
 };
